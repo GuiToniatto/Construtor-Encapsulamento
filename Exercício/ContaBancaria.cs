28eb62dad4b867cf8b7d493485bcb4f3d190c436
Guilherme Toniatto Simões <guilhermetoniatto@hotmail.com>
2021-02-16 16:24:18 -0300

    first commit

---
+++ Exercício/ContaBancaria.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercício
+{
+    class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular) 
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.00;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo = Saldo -5 - valor;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Titular + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture) + "\n";
+        }
+    }
+}
---
+++ Exercício/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Você deve fazer um programa que realize o cadastro de uma conta, dando opção para que seja ou não informado o valor de depósito inicial. " +
+                "Em seguida, realizar um depósito e depois um saque, sempre mostrando os dados da conta após cada operação.");
+
+            ContaBancaria conta;
+
+            Console.Write("Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char opcaoDeposito = char.Parse(Console.ReadLine());
+
+            if (opcaoDeposito == 's' || opcaoDeposito == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double valorDepositado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numeroConta, nome, valorDepositado);
+            }
+            else
+                conta = new ContaBancaria(numeroConta, nome);
+
+            Console.WriteLine("\nDados da conta:");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para depóstio: ");
+            conta.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+            Console.Write("Entre um valor para saque: ");
+            conta.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
